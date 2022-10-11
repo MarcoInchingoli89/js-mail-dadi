22369d1becb45973f040e1edce9dd62a60db42cd
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-11 15:28:59 +0200

    Add Instructions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,28 @@
-/* Mail
-Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
-Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Mail
+
+/* Consegna */
+
+/* Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo. */
+
+/* Istruzioni */
+
+// Chiedi all'utente la mail usando prompt
+// Crea un array di mail di cui una o due saranno valide per l'accesso
+// Fai un controllo nell'array con un ciclo for
+// A seconda dell'esito scrivi un messaggio in console
+
+// Gioco dei dadi
+
+/* Consegna */
+
+/* Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve? */
+
+/* Istruzioni */
+
+// Realizziamo un array da 1 a 6
+// Con un ciclo for verifichiamo l'array
+// Generiamo due variabili, una per il giocatore, una per il computer
+// All'interno del ciclo for creiamo un ciclo if
+// Se un punteggio è maggiore dell'altro vince
+// Stampiamo il risultato in console
